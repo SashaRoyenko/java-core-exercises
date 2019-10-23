b8fe985a8c519fc7cdf8e1809d6857adafc47740
Oleksandr Roienko <Oleksandr_Roienko@epam.com>
2019-10-23 15:37:23 +0300

    Solve FileReaders task

--- file-reader/src/main/java/ua/procamp/FileReaders.java
+++ file-reader/src/main/java/ua/procamp/FileReaders.java
@@ -1,5 +1,13 @@
 package ua.procamp;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 /**
  * {@link FileReaders} privides an API that allow to read whole file into a {@link String} by file name.
  */
@@ -12,6 +20,21 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new UnsupportedOperationException("It's your job to make it work!"); //todo
+        Path filePath = createPathFromFileName(fileName);
+        try {
+            return Files.lines(filePath)
+                    .collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static Path createPathFromFileName(String fileName) {
+        URL fileUrl = FileReaders.class.getClassLoader().getResource(fileName);
+        try {
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException();
+        }
     }
 }
